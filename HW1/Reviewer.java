7d6852fdef406f7bdd72cb03bf8784b79f7434e2
JoseCarlosMunoz12 <josecarlos.munoz12@outlook.com>
2023-06-07 06:39:23 -0500

    Update Reviewer.java
    added setter getters to Reviewer class

--- HW1/Reviewer.java
+++ HW1/Reviewer.java
@@ -2,4 +2,24 @@ public class Reviewer{
 	private int UID;
 	private String name;
 	private String Bio;
+	
+	public void setUID(int uid){
+		this.UID = uid;
+	}
+	public void setName(String name){
+		this.Name = name;
+	}
+	public void setBio(String bio){
+		this.Bio = bio;
+	}
+	
+	public int getUID(){
+		return this.UID;
+	}
+	public String getName(){
+		return this.Name;
+	}
+	public String getBio(){
+		return this.Bio;
+	}
 }
