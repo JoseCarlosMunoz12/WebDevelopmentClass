07972179f345776af7581d22741a793b0449360d
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2023-06-06 22:54:03 -0500

    added SPRING test

---
+++ Week 2/StudenSystem1/src/main/java/edu/roosevelt/jmunoz/StudenSystem1/StudenSystem1Application.java
@@ -0,0 +1,14 @@
+package edu.roosevelt.jmunoz.StudenSystem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudenSystem1Application.class, args);
+	}
+        
+
+}
---
+++ Week 2/StudenSystem1/src/test/java/edu/roosevelt/jmunoz/StudenSystem1/StudenSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.roosevelt.jmunoz.StudenSystem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
