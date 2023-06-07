e93052c2adad52b7a4ded036c455949533b7fd1e
JoseCarlosMunoz12 <josecarlos.munoz12@outlook.com>
2023-06-07 07:31:55 -0500

    added setup for review Controller

---
+++ HW1/ReviewsController.java
@@ -0,0 +1,7 @@
+public class ReviewsController{
+	private ArrayList<Review> Reviews = new ArrayList<Review>();
+	private ArrayList<Reviewer> Critics = new ArrayList<Reviewer>();
+	public ReviewsController(){
+		
+	}
+}
