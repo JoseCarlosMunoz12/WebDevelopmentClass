9f6ed320a0eb06a89da0b0504783e477c066a018
JoseCarlosMunoz12 <josecarlos.munoz12@outlook.com>
2023-06-07 06:38:38 -0500

    Update Review.java
    added setters and getters to review class

--- HW1/Review.java
+++ HW1/Review.java
@@ -3,5 +3,35 @@ public class Review{
 	private String Title;
 	private String Body;
 	private double Start;
-	private Reviewr reviewer
+	private int UID;
+	
+	public void setRID(int rid){
+		this.RID = rid;
+	}
+	public void setTitle(String title){
+		this.Title = title;
+	}
+	public void setBody(String body){
+		this.Body = body;
+	}
+	public void setStars(double stars){
+		this.Stars = stars;
+	}
+	public void setUID(double uid){
+		this.UID = uid;
+	}
+	
+	public int getRID(){
+		return this.RID;
+	}
+	public String getTitle(){
+		return this.Title;
+	}
+	public String getBody(){
+		return this.Body;
+	}
+	public double getStars(){
+		return this.Stars;
+	}
+	public int getUID(){
 }
